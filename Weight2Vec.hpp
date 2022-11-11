f89ced9db7ff46818d0b637176a96d5c04de784f
Taeseong Kim <114977212+taseong@users.noreply.github.com>
2022-11-11 17:49:19 +0900

    Update Weight2Vec.hpp

--- Weight2Vec.hpp
+++ Weight2Vec.hpp
@@ -7,12 +7,13 @@
 
 using namespace std;
 
-void Weight2Vec(string path , int num_output_channel , int num_input_channel, int kernel_size){
+void Weight2Vec(string path , int num_output_channel , int num_input_channel, int kernel_size,
+        vector<vector<vector<double>>> &kernel_weight){
     
     //file read -> vector 
     string line;
     fstream fs;
-    std::vector<string> lines;
+    vector<string> lines;
 
     fs.open(path,ios::in); //input file_Path
     
@@ -21,8 +22,7 @@ void Weight2Vec(string path , int num_output_channel , int num_input_channel, in
         lines.push_back(line);
     }
 
-    vector<vector<vector<double>>> kernel_weight(num_output_channel, vector<vector<double>>(num_input_channel, vector<double>(kernel_size * kernel_size,0)));
-
+   
     int total_num = num_output_channel * num_input_channel * kernel_size * kernel_size;
     for(int i = 0 ; i < num_output_channel; i++){
         for(int j = 0 ; j < num_input_channel ; j++){
@@ -33,17 +33,6 @@ void Weight2Vec(string path , int num_output_channel , int num_input_channel, in
         }
     }
 
-
-    //print vector elements.
-     for(int i = 0 ; i < num_output_channel; i++){
-        for(int j = 0 ; j < num_input_channel ; j++){
-            for(int k = 0 ; k < kernel_size*kernel_size ; k++){
-                    std::cout << "i : " << i+1 << " j : "<< j+1 << " k : " << k+1 << " " ;
-                    std::cout <<  kernel_weight[i][j][k] <<std::endl;
-            }
-            std::cout << std::endl;
-        }
-    }
 
     fs.close();
 }
