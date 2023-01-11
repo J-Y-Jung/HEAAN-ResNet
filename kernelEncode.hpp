d44edd51ceee71db6cea76adf1ccd14924d49651
altaria1 <114068321+altaria1@users.noreply.github.com>
2023-01-11 22:31:29 +0900

    Update kernelEncode.hpp

--- kernelEncode.hpp
+++ kernelEncode.hpp
@@ -42,16 +42,21 @@ void kernel_ptxt(Context context, vector<double>& weight, vector<vector<vector<P
     if (ker_size == 3) {  //output vector is out_ch * in_ch * 9
         for (int i = 0; i < out_ch; ++i) {
             for (int j = 0; j < in_ch; ++j) {
-
+                
+                cout << i<< j << "th loop start..." << endl;
                 vector<vector<double>> temp(3, vector<double>(3, 0));
 
                 for (int k = 0; k < 3; ++k) {
                     for (int l = 0; l < 3; ++l) temp[k][l] = weight[9 * in_ch * i + 9 * j + 3 * k + l];
                 }
+                
+                cout<< "junyoung..." <<endl;
 
                 for (int k = 0; k < 3; ++k) {
                     for (int l = 0; l < 3; ++l) weightToPtxt(output[i][j][3 * k + l], level, temp, gap_in, stride, (u64)k, (u64)l, ecd);
                 }
+                
+                cout << "succeed" <<endl;
             }
         }
     }
@@ -59,13 +64,17 @@ void kernel_ptxt(Context context, vector<double>& weight, vector<vector<vector<P
     if (ker_size == 1) { //output vector is out_ch * in_ch * 1
         for (int i = 0; i < out_ch; ++i) {
             for (int j = 0; j < in_ch; ++j) {
+                
+                cout << "start..." << endl;
 
                 vector<vector<double>> temp(0, vector<double>(0, 0));
                 temp[0][0] = weight[in_ch * i +  j];
+                
+                cout<< "junyoung..." <<endl;
 
-                Plaintext ptxt(context);
-                weightToPtxt(ptxt, level, temp, gap_in, stride, (u64)1, (u64)1, ecd);
-                output[i][j][0] = ptxt;
+                weightToPtxt(output[i][j][0], level, temp, gap_in, stride, (u64)1, (u64)1, ecd);
+                
+                cout << "succeed" <<endl;
 
             }
         }
