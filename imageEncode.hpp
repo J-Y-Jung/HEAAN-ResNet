35e6d5b4e094186535093e12f6c899c72654b422
altaria1 <114068321+altaria1@users.noreply.github.com>
2023-01-11 22:28:46 +0900

    Update imageEncode.hpp

--- imageEncode.hpp
+++ imageEncode.hpp
@@ -56,22 +56,18 @@ void imageCompiler(Context context, KeyPack pack, Encryptor enc, vector<double>&
 
     Ciphertext ctxt1(context), ctxt2(context), ctxt3(context);
     
-    cout << "ready" << endl;
     for (int i = 0; i < 32; ++i) {
         vector<double> temp1 = slice(image, 3072 * i, 3072 * i + 1024);
         vector<double> temp2 = slice(image, 3072 * i + 1024, 3072 * i + 2048);
         vector<double> temp3 = slice(image, 3072 * i + 2048, 3072 * i + 3072);
 
-        cout << "slicing done" << endl;
 
         input1.insert(input1.end(), temp1.begin(), temp1.end());
         input2.insert(input2.end(), temp2.begin(), temp2.end());
         input3.insert(input3.end(), temp3.begin(), temp3.end());
 
-        cout << i << "th for loop done" << endl;
     }
     
-    cout<< "double vector done" <<endl;
 
     for (size_t i = 0; i < num_slots; ++i) {
         msg1[i].real(input1[i]);
@@ -84,7 +80,6 @@ void imageCompiler(Context context, KeyPack pack, Encryptor enc, vector<double>&
         msg3[i].imag(0.0);
     }
     
-    cout<<"encoding msg done"<<endl;
 
     Plaintext ptxt1(context), ptxt2(context), ptxt3(context);
 
@@ -96,5 +91,4 @@ void imageCompiler(Context context, KeyPack pack, Encryptor enc, vector<double>&
     output.push_back(ctxt2);
     output.push_back(ctxt3);
     
-    cout<< "encrypt done" <<endl;
 }
