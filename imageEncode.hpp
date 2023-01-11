97de7c8f68dd425ecaca3150aebe100bb72bf716
altaria1 <114068321+altaria1@users.noreply.github.com>
2023-01-11 20:55:20 +0900

    Update imageEncode.hpp

--- imageEncode.hpp
+++ imageEncode.hpp
@@ -37,7 +37,7 @@ void txtreader(vector<double>& kernel, const string filename) {
 
 vector<double> slice(const std::vector<double>& input, int a, int b) {
     auto first = input.begin() + a;
-    auto last = input.begin() + b;
+    auto last = input.begin() + b+1;
     return std::vector<double>(first, last);
 }
 
@@ -55,16 +55,19 @@ void imageCompiler(Context context, KeyPack pack, Encryptor enc, vector<double>&
     vector<double> input1, input2, input3;
 
     Ciphertext ctxt1(context), ctxt2(context), ctxt3(context);
-
+    
+    cout << "ready" << cout;
     for (int i = 0; i < 32; ++i) {
-        vector<double> temp1 = slice(image, 3072 * i, 3072 * i + 1023);
-        vector<double> temp2 = slice(image, 3072 * i + 1024, 3072 * i + 2047);
-        vector<double> temp3 = slice(image, 3072 * i + 2048, 3072 * i + 3071);
+        vector<double> temp1 = slice(image, 3072 * i, 3072 * i + 1024);
+        vector<double> temp2 = slice(image, 3072 * i + 1024, 3072 * i + 2048);
+        vector<double> temp3 = slice(image, 3072 * i + 2048, 3072 * i + 3072);
 
         input1.insert(input1.end(), temp1.begin(), temp1.end());
         input2.insert(input2.end(), temp2.begin(), temp2.end());
         input3.insert(input3.end(), temp3.begin(), temp3.end());
     }
+    
+    cout<< "double vector done" <<cout;
 
     for (size_t i = 0; i < num_slots; ++i) {
         msg1[i].real(input1[i]);
@@ -76,6 +79,8 @@ void imageCompiler(Context context, KeyPack pack, Encryptor enc, vector<double>&
         msg3[i].real(input3[i]);
         msg3[i].imag(0.0);
     }
+    
+    cout<<"encoding msg done"<<endl;
 
     Plaintext ptxt1(context), ptxt2(context), ptxt3(context);
 
@@ -86,4 +91,6 @@ void imageCompiler(Context context, KeyPack pack, Encryptor enc, vector<double>&
     output.push_back(ctxt1);
     output.push_back(ctxt2);
     output.push_back(ctxt3);
+    
+    cout<< "encrypt done" <<endl;
 }
