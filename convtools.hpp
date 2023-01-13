b0c00976ae4c685c7c7139af1ab00a4c7835b9c9
Junyoung Jung <99154567+J-Y-Jung@users.noreply.github.com>
2023-01-13 16:05:42 +0900

    Add files via upload

--- convtools.hpp
+++ convtools.hpp
@@ -41,7 +41,7 @@ for 1-by-1 kernel, you must set
 weight index = |(1,1)|
 */
 
-void weightToPtxt(Plaintext &ptxt, u64 level, vector<vector<double>> weights,
+void weightToPtxtOld(Plaintext &ptxt, u64 level, vector<vector<double>> weights,
                     u64 gap_in, u64 stride, 
                     u64 weight_row_idx, u64 weight_col_idx, EnDecoder ecd) {
     Message msg(15);                    
@@ -173,8 +173,55 @@ void weightToPtxt(Plaintext &ptxt, u64 level, vector<vector<double>> weights,
     return;
 }
 
+void weightToPtxt(Plaintext &ptxt, u64 level, double weight,
+                    u64 gap_in, 
+                    u64 weight_row_idx, u64 weight_col_idx, EnDecoder ecd) {
+    Message msg(15);                    
+    auto num_slots = msg.getSize();
+    
+    for (size_t j = 0; j < num_slots; ++j){
+        msg[j].real(weight);
+        msg[j].imag(0.0);   
+    }
+	
+    if (weight_row_idx == 0){
+        for (size_t j = 0; j < 32; ++j){
+            for (size_t i = 0; i < 32*gap_in; ++i) {
+                msg[j*1024+i].real(0.0);
+            }
+        }
+    }
+    else if (weight_row_idx == 2){
+        for (size_t j = 0; j < 32; ++j){
+            for (size_t i = 0; i < 32*gap_in; ++i) {
+                msg[j*1024+(1024-32*gap_in)+i].real(0.0);
+            }
+        }
+    }
+    if (weight_col_idx == 0){
+        for (size_t j = 0; j < 32; ++j){
+            for (size_t i = 0; i < 32; ++i) {
+                for (size_t k = 0; k < gap_in; ++k) {
+                    msg[j*1024+i*32+k].real(0.0);
+                }
+            }
+        }
+    }
+    else if (weight_col_idx == 2){
+        for (size_t j = 0; j < 32; ++j){
+            for (size_t i = 0; i < 32; ++i) {
+                for (size_t k = 0; k < gap_in; ++k) {
+                    msg[j*1024+i*32+(31-k)].real(0.0);
+                }
+            }
+        }
+    }
+    ptxt = ecd.encode(msg, level, 0);
+    return;
+}
+
 //weight for the first conv layer
-void weightToPtxt1stConv(Plaintext &ptxt, u64 level, vector<double> weights, 
+void weightToPtxt1stConvOld(Plaintext &ptxt, u64 level, vector<double> weights, 
                     u64 weight_row_idx, u64 weight_col_idx, EnDecoder ecd) {
     Message msg(15);   
     auto num_slots = msg.getSize();
