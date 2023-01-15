e7b45a63f7a7680f146c614e3853754d5282a3dd
Junyoung Jung <99154567+J-Y-Jung@users.noreply.github.com>
2023-01-15 15:55:32 +0900

    Add files via upload

--- leveldown.hpp
+++ leveldown.hpp
@@ -12,7 +12,7 @@ void levelDownBundle(HEaaN::Context context, HEaaN::KeyPack pack, HEaaN::HomEval
 					vector<vector<HEaaN::Ciphertext>>& ctxt_bundle, u64 target_level){
 	u64 d1 = ctxt_bundle.size();
 	u64 d2 = ctxt_bundle[0].size();
-	#pragma omp for collapse(2)
+	#pragma omp parallel for collapse(2)
 	for (u64 i; i<d1; i++){
 		for (u64 j; j<d2; j++){
 			eval.levelDown(ctxt_bundle[i][j], target_level, ctxt_bundle[i][j]);
