e404ec34961f46378584ef24c488008ead567eb6
YongdongYEO <110511305+YongdongYEO@users.noreply.github.com>
2023-01-15 12:55:30 +0900

    Update RB+BN.hpp

--- RB+BN.hpp
+++ RB+BN.hpp
@@ -1,4 +1,5 @@
 #include "kernelEncode.hpp"
+#include <math.h>
 
 namespace {
 using namespace HEaaN;
@@ -29,16 +30,16 @@ vector<Plaintext>& BN2_add) {
     ///////////////////////// Main flow /////////////////////////////////////////
     std::cout << "First Conv-(main flow) ..." << std::endl;
     std::vector<std::vector<HEaaN::Ciphertext>> ctxt_conv_out_bundle;
-    for (int i = 0; i < 16/(4*DSB_count); ++i) { // 서로 다른 img
+    for (int i = 0; i < 16/pow(4, DSB_count); ++i) { // 서로 다른 img
         //std::cout << "i = " << i << std::endl;
         std::vector<HEaaN::Ciphertext> ctxt_conv_out_cache;
-        ctxt_conv_out_cache = Conv(context, pack, eval, 32, 1, 1, 16*(2*DSB_count), 16*(2*DSB_count), ctxt_bundle[i], kernel_bundle);
+        ctxt_conv_out_cache = Conv(context, pack, eval, 32, 1, 1, 16*pow(2, DSB_count), 16*pow(2, DSB_count), ctxt_bundle[i], kernel_bundle);
         ctxt_conv_out_bundle.push_back(ctxt_conv_out_cache);
     }
     std::cout << "DONE!" << "\n";
 
     cout << "Adding BN-(main flow) ..." << endl;
-    addBNsummands(context, eval,ctxt_conv_out_bundle, BN1_add, 16, 16);
+    addBNsummands(context, eval,ctxt_conv_out_bundle, BN1_add, 16/pow(4, DSB_count), 16*pow(2, DSB_count)); // 마지막 두개가 뭐지..
     cout << "DONE!" << "\n";
 
 
@@ -51,9 +52,9 @@ vector<Plaintext>& BN2_add) {
     // AppReLU
     std::cout << "AppReLU-(main flow) ..." << std::endl;
     std::vector<std::vector<HEaaN::Ciphertext>> ctxt_relu_out_bundle;
-    for (int i = 0; i < 16/(4*DSB_count); ++i) {
+    for (int i = 0; i < 16/pow(4, DSB_count); ++i) {
         std::vector<HEaaN::Ciphertext> ctxt_relu_out_allch_bundle;
-        for (int ch = 0; ch < 16*(2*DSB_count); ++ch) {
+        for (int ch = 0; ch < 16*pow(2, DSB_count); ++ch) {
             std::cout << ch << "\n";
             HEaaN::Ciphertext ctxt_relu_out(context);
             ApproxReLU(context, eval, ctxt_conv_out_bundle[i][ch], ctxt_relu_out);
@@ -68,9 +69,9 @@ vector<Plaintext>& BN2_add) {
     // Second convolution
     std::cout << "Second Conv-(main flow) ..." << std::endl;
     std::vector<std::vector<HEaaN::Ciphertext>> ctxt_conv_out2_bundle;
-    for (int i = 0; i < 16/(4*DSB_count); ++i) {
+    for (int i = 0; i < 16/pow(4, DSB_count); ++i) {
         std::vector<HEaaN::Ciphertext> ctxt_conv_out2_allch_bundle;
-        ctxt_conv_out2_allch_bundle = Conv(context, pack, eval, 32, 1, 1, 16*(2*DSB_count), 16*(2*DSB_count), ctxt_relu_out_bundle[i], kernel_bundle2);
+        ctxt_conv_out2_allch_bundle = Conv(context, pack, eval, 32, 1, 1, 16*pow(2, DSB_count), 16*pow(2, DSB_count), ctxt_relu_out_bundle[i], kernel_bundle2);
         ctxt_conv_out2_bundle.push_back(ctxt_conv_out2_allch_bundle);
     }
     ctxt_relu_out_bundle.clear();
@@ -78,16 +79,16 @@ vector<Plaintext>& BN2_add) {
     std::cout << "DONE!" << "\n";
 
     cout << "Adding BN-(main flow) ..." << endl;
-    addBNsummands(context, eval,ctxt_conv_out2_bundle, BN2_add, 16, 16);
+    addBNsummands(context, eval,ctxt_conv_out2_bundle, BN2_add, 16/pow(4, DSB_count), 16*pow(2, DSB_count));
     cout << "DONE!" << "\n";
 
 
     //////////////////////////// Main flow + Residual flow //////////////////////////////////
     std::cout << "Main flow + Residual flow ..." << std::endl;
     std::vector<std::vector<HEaaN::Ciphertext>> ctxt_residual_added;
-    for (int i = 0; i < 16; ++i) {
+    for (int i = 0; i < 16/pow(4, DSB_count); ++i) {
         std::vector<HEaaN::Ciphertext> ctxt_residual_added_allch_bundle;
-        for (int ch = 0; ch < 16; ++ch) {
+        for (int ch = 0; ch < 16*pow(2, DSB_count); ++ch) {
             HEaaN::Ciphertext ctxt_residual_added_cache(context);
             eval.add(ctxt_conv_out2_bundle[i][ch], ctxt_bundle[i][ch], ctxt_residual_added_cache);
             ctxt_residual_added_allch_bundle.push_back(ctxt_residual_added_cache);
@@ -99,9 +100,9 @@ vector<Plaintext>& BN2_add) {
     // Last AppReLU
     std::cout << "Last AppReLU ..." << std::endl;
     std::vector<std::vector<HEaaN::Ciphertext>> ctxt_RB_out;
-    for (int i = 0; i < 16/(4*DSB_count); ++i) {
+    for (int i = 0; i < 16/pow(4, DSB_count); ++i) {
         std::vector<HEaaN::Ciphertext> ctxt_RB_out_allch_bundle;
-        for (int ch = 0; ch < 16*(2*DSB_count); ++ch) {
+        for (int ch = 0; ch < 16*pow(2, DSB_count); ++ch) {
             HEaaN::Ciphertext ctxt_RB_out_cache(context);
             ApproxReLU(context, eval, ctxt_residual_added[i][ch], ctxt_RB_out_cache);
             ctxt_RB_out_allch_bundle.push_back(ctxt_RB_out_cache);
