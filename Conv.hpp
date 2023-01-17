b7c972f82d0e274cacf721c73a360c2755d39ef4
altaria1 <114068321+altaria1@users.noreply.github.com>
2023-01-17 20:44:13 +0900

    Update Conv.hpp

--- Conv.hpp
+++ Conv.hpp
@@ -28,6 +28,15 @@ Ciphertext auxiliaryFtn1(HomEvaluator eval, Context context, vector<Ciphertext>&
     for (int inputid = 0; inputid < input_channel; ++inputid) {
 
         HEaaN::Ciphertext ctxt_out_cache(context);
+
+        int level1 = ctxtVec[inputid].getLevel();
+        int level2 = ptxtVec[inputid][0].getLevel();
+        if (level1 > level2) {
+            eval.levelDown(ctxtVec[inputid], level2, ctxtVec[inputid]);
+        }
+        else if (level1 < level2) {
+            ptxtVec[inputid][0].setLevel(level1);
+        }
         eval.multWithoutRescale(ctxtVec[inputid], ptxtVec[inputid][0], ctxt_out_cache);
 
         if (inputid == 0) {
@@ -52,10 +61,29 @@ Ciphertext auxiliaryFtn9(HomEvaluator eval, Context context, vector<vector<Ciphe
 
         Ciphertext ctxt_out_cache(context);
 
+
+
+        int level1 = ctxtVec[inputid][0].getLevel();
+        int level2 = ptxtVec[inputid][0].getLevel();
+        if (level1 > level2) {
+            eval.levelDown(ctxtVec[inputid][0], level2, ctxtVec[inputid][0]);
+        }
+        else if (level1 < level2) {
+            ptxtVec[inputid][0].setLevel(level1);
+        }
+
         eval.multWithoutRescale(ctxtVec[inputid][0], ptxtVec[inputid][0], ctxt_out_cache);
 
         Ciphertext mult_cache(context);
         for (int i = 1; i < 9; ++i) {
+            int level1 = ctxtVec[inputid][i].getLevel();
+            int level2 = ptxtVec[inputid][i].getLevel();
+            if (level1 > level2) {
+                eval.levelDown(ctxtVec[inputid][i], level2, ctxtVec[inputid][i]);
+            }
+            else if (level1 < level2) {
+                ptxtVec[inputid][i].setLevel(level1);
+            }
             eval.multWithoutRescale(ctxtVec[inputid][i], ptxtVec[inputid][i], mult_cache);
             eval.add(ctxt_out_cache, mult_cache, ctxt_out_cache);
         }
