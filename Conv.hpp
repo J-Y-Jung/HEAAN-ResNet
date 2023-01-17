eb1f466a21dd579f2a26479b196f163097ceeec2
altaria1 <114068321+altaria1@users.noreply.github.com>
2023-01-17 17:08:05 +0900

    Update Conv.hpp

--- Conv.hpp
+++ Conv.hpp
@@ -1,34 +1,92 @@
-namespace {
-    using namespace std;
-    using namespace HEaaN;
-}
+#pragma once
+#include <iostream>
+#include <vector>
+#include <omp.h>
+#include <cmath>
 
+namespace {
+    using namespace HEaaN;
+    using namespace std;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
 // Copyleft (C) 2021-2022 Crypto Lab Inc.                                     //
 //                                                                            //
 // - This file is part of HEaaN homomorphic encryption library.               //
-// - HEaaN cannot be copied and/or distributed without the express permission //
+// - HEaaN cannot be copied and/or distributed Withno the express permission //
 //  of Crypto Lab Inc.                                                        //
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
 
 
-std::vector<HEaaN::Ciphertext> Conv(HEaaN::Context context, HEaaN::KeyPack pack,
-HEaaN::HomEvaluator eval, int imgsize, int gap, int stride, int input_channel, int output_channel, 
-std::vector<HEaaN::Ciphertext>& ctxt_bundle, 
-std::vector<std::vector<std::vector<HEaaN::Plaintext>>>& kernel_o) {
+Ciphertext auxiliaryFtn1(HomEvaluator eval, Context context, vector<Ciphertext>& ctxtVec, vector<vector<Plaintext>>& ptxtVec, int input_channel) {
 
-    int kernelsize;
-    kernelsize = kernel_o[0][0].size();
+    Ciphertext ctxt_out(context);
+
+    for (int inputid = 0; inputid < input_channel; ++inputid) {
+
+        HEaaN::Ciphertext ctxt_out_cache(context);
+        eval.multWithoutRescale(ctxtVec[inputid], ptxtVec[inputid][0], ctxt_out_cache);
+
+        if (inputid == 0) {
+            ctxt_out = ctxt_out_cache;
+        }
+
+        else {
+            eval.add(ctxt_out, ctxt_out_cache, ctxt_out);
+        }
+    }
+    eval.rescale(ctxt_out);
+
+    return ctxt_out;
+}
+
+
+Ciphertext auxiliaryFtn9(HomEvaluator eval, Context context, vector<vector<Ciphertext>>& ctxtVec, vector<vector<Plaintext>>& ptxtVec, int input_channel) {
+
+    Ciphertext ctxt_out(context);
+
+    for (int inputid = 0; inputid < input_channel; ++inputid) {
+
+        Ciphertext ctxt_out_cache(context);
+
+        eval.multWithoutRescale(ctxtVec[inputid][0], ptxtVec[inputid][0], ctxt_out_cache);
+
+        Ciphertext mult_cache(context);
+        for (int i = 1; i < 9; ++i) {
+            eval.multWithoutRescale(ctxtVec[inputid][i], ptxtVec[inputid][i], mult_cache);
+            eval.add(ctxt_out_cache, mult_cache, ctxt_out_cache);
+        }
+
+        if (inputid == 0) {
+            ctxt_out = ctxt_out_cache;
+        }
+
+        else {
+            eval.add(ctxt_out, ctxt_out_cache, ctxt_out);
+        }
+    }
+    eval.rescale(ctxt_out);
+
+    return ctxt_out;
+}
+
+
+std::vector<HEaaN::Ciphertext> Conv_parallel(HEaaN::Context context, HEaaN::KeyPack pack,
+    HEaaN::HomEvaluator eval, int imgsize, int gap, int stride, int input_channel, int output_channel,
+    std::vector<HEaaN::Ciphertext>& ctxt_bundle,
+    std::vector<std::vector<std::vector<HEaaN::Plaintext>>>& kernel_o) {
+
+    int kernelsize = kernel_o[0][0].size();
 
     HEaaN::Ciphertext ctxt_init(context);
 
-    std::vector<HEaaN::Ciphertext> ctxt_out_bundle;
-    // 비슷한 방법으로 input_channel은 ctxt_bundle에서 읽어올 수 있음.
+    std::vector<HEaaN::Ciphertext> ctxt_out_bundle(output_channel, ctxt_init);
 
+
+    // Convolution
 
     if (kernelsize == 9) {
 
@@ -43,70 +101,24 @@ std::vector<std::vector<std::vector<HEaaN::Plaintext>>>& kernel_o) {
             }
         }
 
+        //#pragma omp parallel for
         for (int outputid = 0; outputid < output_channel; ++outputid) {
-
-            HEaaN::Ciphertext ctxt_out(context);
-
-            for (int inputid = 0; inputid < (input_channel); ++inputid) {
-
-                HEaaN::Ciphertext ctxt_out_cache(context);
-
-                eval.multWithoutRescale(rotated_ctxts_bundle[inputid][0], kernel_o[outputid][inputid][0], ctxt_out_cache);
-
-                HEaaN::Ciphertext mult_cache(context);
-                for (int i = 1; i < 9; ++i) {
-                    eval.multWithoutRescale(rotated_ctxts_bundle[inputid][i], kernel_o[outputid][inputid][i], mult_cache);
-                    eval.add(ctxt_out_cache, mult_cache, ctxt_out_cache);
-                }
-
-
-                if (inputid == 0) {
-                    ctxt_out = ctxt_out_cache;
-                }
-                else {
-                    eval.add(ctxt_out, ctxt_out_cache, ctxt_out);
-                }
-            }
-
-            eval.rescale(ctxt_out);
-            ctxt_out_bundle.push_back(ctxt_out);
+            ctxt_out_bundle[outputid] = auxiliaryFtn9(eval, context, rotated_ctxts_bundle, kernel_o[outputid], input_channel);
         }
 
         rotated_ctxts_bundle.clear();
         rotated_ctxts_bundle.shrink_to_fit();
 
     }
-    
 
-    if (kernelsize == 1) {
+    else {
 
+        #pragma omp parallel for
         for (int outputid = 0; outputid < output_channel; ++outputid) {
-            // std::cout << outputid << " out\n";
-
-            HEaaN::Ciphertext ctxt_out(context);
-
-            for (int inputid = 0; inputid < (input_channel); ++inputid) {
-
-                HEaaN::Ciphertext ctxt_out_cache(context);
-                eval.multWithoutRescale(ctxt_bundle[inputid], kernel_o[outputid][inputid][0], ctxt_out_cache);
-
-                if (inputid == 0) {
-                    ctxt_out = ctxt_out_cache;
-                }
-                else {
-                    eval.add(ctxt_out, ctxt_out_cache, ctxt_out);
-                }
-            }
-            eval.rescale(ctxt_out);
-
-            ctxt_out_bundle.push_back(ctxt_out);
+            ctxt_out_bundle[outputid] = auxiliaryFtn1(eval, context, ctxt_bundle, kernel_o[outputid], input_channel);
         }
-
-
     }
 
-
     return ctxt_out_bundle;
+
 }
-
-
