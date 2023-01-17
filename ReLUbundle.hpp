f8cdc4eaba2b99f97d2a1bb88e268e5987072cfa
Taeseong Kim <114977212+taseong@users.noreply.github.com>
2023-01-17 23:23:13 +0900

    Add files via upload

--- ReLUbundle.hpp
+++ ReLUbundle.hpp
@@ -179,12 +179,12 @@ void ApproxReLU_bundle(HEaaN::Context context, HEaaN::KeyPack pack,HEaaN::HomEva
     std::vector<std::vector<HEaaN::Ciphertext>>& ctxt_bundle, std::vector<std::vector<HEaaN::Ciphertext>>& ctxt_relu_bundle){
     
     HEaaN::Ciphertext ctxt_temp(context); //for initializing
-    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_temp_bundle(ctxt_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_bundle[0].size(),ctxt_temp));
+    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_temp_bundle(ctxt_relu_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_relu_bundle[0].size(),ctxt_temp));
     
     //std::cout << "size1 = " << ctxt_bundle.size() << " size2 = " << ctxt_bundle[0].size() << std::endl;
     #pragma omp parallel for collapse(2)
-    for(int i = 0 ; i < ctxt_bundle.size() ; i++){
-        for(int j = 0 ; j < ctxt_bundle[0].size() ; j++){
+    for(int i = 0 ; i < ctxt_relu_bundle.size() ; i++){
+        for(int j = 0 ; j < ctxt_relu_bundle[0].size() ; j++){
             eval.conjugate(ctxt_bundle[i][j],ctxt_temp_bundle[i][j]);
             eval.add(ctxt_temp_bundle[i][j],ctxt_bundle[i][j],ctxt_temp_bundle[i][j]);
             eval.mult(ctxt_temp_bundle[i][j],0.5,ctxt_temp_bundle[i][j]);
@@ -192,11 +192,11 @@ void ApproxReLU_bundle(HEaaN::Context context, HEaaN::KeyPack pack,HEaaN::HomEva
     }
 
     ///std::cout << "b" << std::endl;
-    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_real_BTS_bundle(ctxt_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_bundle[0].size(),ctxt_temp));
+    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_real_BTS_bundle(ctxt_relu_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_relu_bundle[0].size(),ctxt_temp));
     //#pragma omp parallel for
-    for(int i = 0 ; i < ctxt_bundle.size() ; ++i){
+    for(int i = 0 ; i < ctxt_relu_bundle.size() ; ++i){
         //#pragma omp parallel for
-        for(int j = 0 ; j < ctxt_bundle[0].size() ; ++j){
+        for(int j = 0 ; j < ctxt_relu_bundle[0].size() ; ++j){
             eval.bootstrap(ctxt_temp_bundle[i][j],ctxt_real_BTS_bundle[i][j],true);
         }
     }
@@ -226,7 +226,7 @@ void ApproxReLU_bundle(HEaaN::Context context, HEaaN::KeyPack pack,HEaaN::HomEva
     };
 
     //std::cout << "c" << std::endl;
-    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_temp1_bundle(ctxt_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_bundle[0].size(),ctxt_temp));
+    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_temp1_bundle(ctxt_relu_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_relu_bundle[0].size(),ctxt_temp));
     #pragma omp parallel for collapse(2)
     for(int i = 0 ; i < ctxt_temp1_bundle.size() ; ++i){
         for(int j = 0 ; j < ctxt_temp1_bundle[0].size() ; ++j){
@@ -235,7 +235,7 @@ void ApproxReLU_bundle(HEaaN::Context context, HEaaN::KeyPack pack,HEaaN::HomEva
     }
     
     //std::cout << "d" << std::endl;
-    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_temp2_bundle(ctxt_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_bundle[0].size(),ctxt_temp));
+    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_temp2_bundle(ctxt_relu_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_relu_bundle[0].size(),ctxt_temp));
     #pragma omp parallel for collapse(2)
     for(int i = 0 ; i < ctxt_temp2_bundle.size() ; ++i){
         for(int j = 0 ; j < ctxt_temp2_bundle[0].size() ; ++j){
@@ -247,7 +247,7 @@ void ApproxReLU_bundle(HEaaN::Context context, HEaaN::KeyPack pack,HEaaN::HomEva
 
     //BTS...
     //std::cout << "BTS..." << std::endl;
-    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_real_BTS2_bundle(ctxt_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_bundle[0].size(),ctxt_temp));
+    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_real_BTS2_bundle(ctxt_relu_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_relu_bundle[0].size(),ctxt_temp));
     //#pragma omp parallel for collapse(2)
     for(int i = 0 ; i < ctxt_real_BTS2_bundle.size() ; ++i){
         //#pragma omp parallel for
@@ -259,7 +259,7 @@ void ApproxReLU_bundle(HEaaN::Context context, HEaaN::KeyPack pack,HEaaN::HomEva
     ctxt_temp2_bundle.shrink_to_fit();
 
     //std::cout << "e" << std::endl;
-    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_sign_bundle(ctxt_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_bundle[0].size(),ctxt_temp));
+    std::vector<std::vector<HEaaN::Ciphertext>> ctxt_sign_bundle(ctxt_relu_bundle.size() , std::vector<HEaaN::Ciphertext>(ctxt_relu_bundle[0].size(),ctxt_temp));
     #pragma omp parallel for collapse(2)
     for(int i = 0 ; i < ctxt_sign_bundle.size() ; ++i){
         for(int j = 0 ; j < ctxt_sign_bundle[0].size() ; ++j){
@@ -271,8 +271,8 @@ void ApproxReLU_bundle(HEaaN::Context context, HEaaN::KeyPack pack,HEaaN::HomEva
 
     //std::cout << "f" << std::endl;
     //#pragma omp parallel for collapse(2)
-    for(int i = 0 ; i < ctxt_bundle.size() ; ++i){
-        for(int j = 0 ; j < ctxt_bundle[0].size() ; ++j){
+    for(int i = 0 ; i < ctxt_relu_bundle.size() ; ++i){
+        for(int j = 0 ; j < ctxt_relu_bundle[0].size() ; ++j){
             eval.mult(ctxt_real_BTS_bundle[i][j] , 0.5 , ctxt_real_BTS_bundle[i][j]);
             eval.mult(ctxt_real_BTS_bundle[i][j],ctxt_sign_bundle[i][j],ctxt_relu_bundle[i][j]);
             eval.add(ctxt_real_BTS_bundle[i][j],ctxt_relu_bundle[i][j],ctxt_relu_bundle[i][j]);
@@ -287,126 +287,127 @@ void ApproxReLU_bundle(HEaaN::Context context, HEaaN::KeyPack pack,HEaaN::HomEva
 }
 
 
-    //Aproximated ReLU function.
-    void ApproxReLU(HEaaN::Context context, HEaaN::HomEvaluator eval, HEaaN::Ciphertext& ctxt, HEaaN::Ciphertext& ctxt_relu) {
+//     //Aproximated ReLU function.
+//     void ApproxReLU(HEaaN::Context context, HEaaN::HomEvaluator eval, HEaaN::Ciphertext& ctxt, HEaaN::Ciphertext& ctxt_relu) {
 
-    // std::cout << "Input ciphertext - level " << ctxt.getLevel()
-    //     << std::endl
-    //     << std::endl;
+//     // std::cout << "Input ciphertext - level " << ctxt.getLevel()
+//     //     << std::endl
+//     //     << std::endl;
 
-    HEaaN::Ciphertext ctxt_temp(context);
-    eval.conjugate(ctxt, ctxt_temp);
-    eval.add(ctxt_temp, ctxt, ctxt_temp);
-    eval.mult(ctxt_temp, 0.5, ctxt_temp);
+//     HEaaN::Ciphertext ctxt_temp(context);
+//     eval.conjugate(ctxt, ctxt_temp);
+//     eval.add(ctxt_temp, ctxt, ctxt_temp);
+//     eval.mult(ctxt_temp, 0.5, ctxt_temp);
 
-    HEaaN::Ciphertext ctxt_real_BTS(context);
-    std::cout << "Imaginary Removal Bootstrapping ... " << std::endl;
+//     HEaaN::Ciphertext ctxt_real_BTS(context);
+//     std::cout << "Imaginary Removal Bootstrapping ... " << std::endl;
 
-    eval.bootstrap(ctxt_temp, ctxt_real_BTS, true);
+//     eval.bootstrap(ctxt_temp, ctxt_real_BTS, true);
 
-    /*std::cout << "Result ciphertext after imaginary removal bootstrapping - level "
-        << ctxt_real_BTS.getLevel() << std::endl
-        << std::endl;*/
+//     /*std::cout << "Result ciphertext after imaginary removal bootstrapping - level "
+//         << ctxt_real_BTS.getLevel() << std::endl
+//         << std::endl;*/
 
-    std::vector<double> polynomial_1 = {
-    1.34595769293910e-33, 2.45589415425004e1, 4.85095667238242e-32, -6.69660449716894e2,
-    -2.44541235853840e-30, 6.67299848301339e3, 1.86874811944640e-29, -3.06036656163898e4,
-    -5.76227817577242e-29, 7.31884032987787e4, 8.53680673009259e-29, -9.44433217050084e4,
-    -6.02701474694667e-29, 6.23254094212546e4, 1.62342843661940e-29, -1.64946744117805e4
-    };
+//     std::vector<double> polynomial_1 = {
+//     1.34595769293910e-33, 2.45589415425004e1, 4.85095667238242e-32, -6.69660449716894e2,
+//     -2.44541235853840e-30, 6.67299848301339e3, 1.86874811944640e-29, -3.06036656163898e4,
+//     -5.76227817577242e-29, 7.31884032987787e4, 8.53680673009259e-29, -9.44433217050084e4,
+//     -6.02701474694667e-29, 6.23254094212546e4, 1.62342843661940e-29, -1.64946744117805e4
+//     };
 
-    /*
-    // product 1/2 to coeff of p_2 because we need to calculate 1/2(x+bar(x)) to do BTS.
-    for(int  i = 0 ; i < polynomial_1.size() ; ++i){
-        polynomial_1[i] = polynomial_1[i]* 0.5;
-    }
-    */
+//     /*
+//     // product 1/2 to coeff of p_2 because we need to calculate 1/2(x+bar(x)) to do BTS.
+//     for(int  i = 0 ; i < polynomial_1.size() ; ++i){
+//         polynomial_1[i] = polynomial_1[i]* 0.5;
+//     }
+//     */
 
-    std::vector<double> polynomial_2 = {
-    1.53261588585630e-47, 9.35625636035439, -3.68972123048249e-46, -5.91638963933626e1,
-    1.74254399703303e-45 , 1.48860930626448e2, -3.20672110002213e-45, -1.75812874878582e2,
-    2.79115738948645e-45, 1.09111299685955e2, -1.22590309306100e-45, -3.66768839978755e1,
-    2.62189142557962e-46, 6.31846290311294, -2.16662326421275e-47, -4.37113415082177e-01
-    };
+//     std::vector<double> polynomial_2 = {
+//     1.53261588585630e-47, 9.35625636035439, -3.68972123048249e-46, -5.91638963933626e1,
+//     1.74254399703303e-45 , 1.48860930626448e2, -3.20672110002213e-45, -1.75812874878582e2,
+//     2.79115738948645e-45, 1.09111299685955e2, -1.22590309306100e-45, -3.66768839978755e1,
+//     2.62189142557962e-46, 6.31846290311294, -2.16662326421275e-47, -4.37113415082177e-01
+//     };
 
-    /*
-        // product 1/2 to coeff of p_2 because we need to calculate 1/2(x+bar(x)) to do BTS.
-        for(int  i = 0 ; i < polynomial_2.size() ; ++i){
-            polynomial_2[i] = polynomial_2[i]*0.5;
-        }
-    */
-    std::vector<double> polynomial_3 = {
-    6.43551938319983e-48, 5.07813569758861, 8.12601038855762e-46, -3.07329918137186e1,
-    -1.60198474678427e-44, 1.44109746812809e2, 1.07463154460511e-43, -4.59661688826142e2,
-    -3.63448723044512e-43, 1.02152064470459e3, 7.25207125369784e-43, -1.62056256708877e3,
-    -9.27306397853655e-43, 1.86467646416570e3, 7.95843097354065e-43, -1.56749300877143e3,
-    -4.69190103147527e-43, 9.60970309093422e2, 1.90863349654016e-43, -4.24326161871646e2,
-    -5.27439678020696e-44, 1.31278509256003e2, 9.47044937974786e-45, -2.69812576626115e1,
-    -9.98181561763750e-46, 3.30651387315565, 4.69390466192199e-47, -1.82742944627533e-1
-    };
+//     /*
+//         // product 1/2 to coeff of p_2 because we need to calculate 1/2(x+bar(x)) to do BTS.
+//         for(int  i = 0 ; i < polynomial_2.size() ; ++i){
+//             polynomial_2[i] = polynomial_2[i]*0.5;
+//         }
+//     */
+//     std::vector<double> polynomial_3 = {
+//     6.43551938319983e-48, 5.07813569758861, 8.12601038855762e-46, -3.07329918137186e1,
+//     -1.60198474678427e-44, 1.44109746812809e2, 1.07463154460511e-43, -4.59661688826142e2,
+//     -3.63448723044512e-43, 1.02152064470459e3, 7.25207125369784e-43, -1.62056256708877e3,
+//     -9.27306397853655e-43, 1.86467646416570e3, 7.95843097354065e-43, -1.56749300877143e3,
+//     -4.69190103147527e-43, 9.60970309093422e2, 1.90863349654016e-43, -4.24326161871646e2,
+//     -5.27439678020696e-44, 1.31278509256003e2, 9.47044937974786e-45, -2.69812576626115e1,
+//     -9.98181561763750e-46, 3.30651387315565, 4.69390466192199e-47, -1.82742944627533e-1
+//     };
     
     
-    //for optimization
-    for(int  i = 0 ; i < 28 ; ++i){
-        polynomial_3[i] = polynomial_3[i] * 0.5;
-    }
+//     //for optimization
+//     for(int  i = 0 ; i < 28 ; ++i){
+//         polynomial_3[i] = polynomial_3[i] * 0.5;
+//     }
 
 
-    HEaaN::Ciphertext ctxt_temp1(context);
+//     HEaaN::Ciphertext ctxt_temp1(context);
 
-    /*std::cout << "1st polynomial evaluation ... " << std::endl << std::endl;*/
-    evalOddPolynomial(context, eval, ctxt_real_BTS, ctxt_temp1, polynomial_1, 4, 2);
-    /*std::cout << "done" << std::endl << std::endl;
+//     /*std::cout << "1st polynomial evaluation ... " << std::endl << std::endl;*/
+//     evalOddPolynomial(context, eval, ctxt_real_BTS, ctxt_temp1, polynomial_1, 4, 2);
+//     /*std::cout << "done" << std::endl << std::endl;
 
-    std::cout << "Ciphertext after evaluating 1st polynomial - level " << ctxt_temp1.getLevel()
-        << std::endl
-        << std::endl;
+//     std::cout << "Ciphertext after evaluating 1st polynomial - level " << ctxt_temp1.getLevel()
+//         << std::endl
+//         << std::endl;
 
-    std::cout << "2nd polynomial evaluation ... " << std::endl << std::endl;*/
+//     std::cout << "2nd polynomial evaluation ... " << std::endl << std::endl;*/
 
-    HEaaN::Ciphertext ctxt_temp2(context);
-    evalOddPolynomial(context, eval, ctxt_temp1, ctxt_temp2, polynomial_2, 2, 3);
+//     HEaaN::Ciphertext ctxt_temp2(context);
+//     evalOddPolynomial(context, eval, ctxt_temp1, ctxt_temp2, polynomial_2, 2, 3);
 
-    /*std::cout << "done" << std::endl << std::endl;
+//     /*std::cout << "done" << std::endl << std::endl;
 
-    std::cout << "Ciphertext after evaluating 2nd polynomial - level " << ctxt_temp2.getLevel()
-        << std::endl
-        << std::endl;*/
+//     std::cout << "Ciphertext after evaluating 2nd polynomial - level " << ctxt_temp2.getLevel()
+//         << std::endl
+//         << std::endl;*/
 
-    /*std::cout << "Bootstrapping ... " << std::endl;*/
-    HEaaN::Ciphertext ctxt_BTS(context);
-    eval.bootstrap(ctxt_temp2, ctxt_BTS, true);
+//     /*std::cout << "Bootstrapping ... " << std::endl;*/
+//     HEaaN::Ciphertext ctxt_BTS(context);
+//     eval.bootstrap(ctxt_temp2, ctxt_BTS, true);
 
-    /*std::cout << "Result ciphertext after bootstrapping - level "
-        << ctxt_BTS.getLevel() << std::endl
-        << std::endl;*/
+//     /*std::cout << "Result ciphertext after bootstrapping - level "
+//         << ctxt_BTS.getLevel() << std::endl
+//         << std::endl;*/
 
-    //ctxt_out3 = eval.sign(ctxt)
+//     //ctxt_out3 = eval.sign(ctxt)
 
-  /*  std::cout << "3rd polynomial evaluation ... " << std::endl << std::endl;*/
+//   /*  std::cout << "3rd polynomial evaluation ... " << std::endl << std::endl;*/
 
-    HEaaN::Ciphertext ctxt_sign(context);
-    evalOddPolynomial(context, eval, ctxt_BTS, ctxt_sign, polynomial_3, 4, 3);
+//     HEaaN::Ciphertext ctxt_sign(context);
+//     evalOddPolynomial(context, eval, ctxt_BTS, ctxt_sign, polynomial_3, 4, 3);
 
-  /*  std::cout << "Ciphertext after evaluating 3rd polynomial - level " << ctxt_sign.getLevel()
-        << std::endl
-        << std::endl;*/
+//   /*  std::cout << "Ciphertext after evaluating 3rd polynomial - level " << ctxt_sign.getLevel()
+//         << std::endl
+//         << std::endl;*/
 
-    //ReLU(x)= 0.5(x+x*sign(x)) = 0.5x + x * (0.5 g3 ^ g2 ^g1)(x), x=ctxt_real_BTS;
-    //ctxt_sign = (0.5 g3 ^ g2 ^g1)(ctxt_real_BTS);
+//     //ReLU(x)= 0.5(x+x*sign(x)) = 0.5x + x * (0.5 g3 ^ g2 ^g1)(x), x=ctxt_real_BTS;
+//     //ctxt_sign = (0.5 g3 ^ g2 ^g1)(ctxt_real_BTS);
     
-    HEaaN::Ciphertext ctxt_half(context);
-    eval.mult(ctxt_real_BTS, 0.5, ctxt_half);
-    eval.mult(ctxt_real_BTS, ctxt_sign, ctxt_relu);
-    eval.add(ctxt_half, ctxt_relu, ctxt_relu);
+//     HEaaN::Ciphertext ctxt_half(context);
+//     eval.mult(ctxt_real_BTS, 0.5, ctxt_half);
+//     eval.mult(ctxt_real_BTS, ctxt_sign, ctxt_relu);
+//     eval.add(ctxt_half, ctxt_relu, ctxt_relu);
     
     
-    //eval.mult(ctxt_real_BTS, 0.5, ctxt_real_BTS);
-    //eval.mult(ctxt_real_BTS, ctxt_sign, ctxt_relu);
-    //eval.add(ctxt_real_BTS, ctxt_relu, ctxt_relu);
+//     //eval.mult(ctxt_real_BTS, 0.5, ctxt_real_BTS);
+//     //eval.mult(ctxt_real_BTS, ctxt_sign, ctxt_relu);
+//     //eval.add(ctxt_real_BTS, ctxt_relu, ctxt_relu);
 
-    // std::cout << "Evaluating Apporximate ReLU done; Ciphertext after evaluating approximate ReLU - level " << ctxt_relu.getLevel()
-    //     << std::endl
-    //     << std::endl;
-}
+//     // std::cout << "Evaluating Apporximate ReLU done; Ciphertext after evaluating approximate ReLU - level " << ctxt_relu.getLevel()
+//     //     << std::endl
+//     //     << std::endl;
+// }
+
 
