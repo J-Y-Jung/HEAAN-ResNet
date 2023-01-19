99a77f7686f721bba689346c696c7b866522fd6d
Junyoung Jung <99154567+J-Y-Jung@users.noreply.github.com>
2023-01-19 11:19:27 +0900

    Update convtoolstest.cpp

--- convtoolstest.cpp
+++ convtoolstest.cpp
@@ -1,8 +1,3 @@
-/*
-Todo : 
-integrate with I/O
-make a function that takes weights, BN values, scaling constants and outputs modified CNN weights
-*/
 
 #include <iostream>
 #include "HEaaN/heaan.hpp"
@@ -53,76 +48,34 @@ int main() {
 
     {
         Plaintext ptxt(context);
-        Message wgt(log_slots);
-        vector<double> v0(32);
-        for (size_t i = 0; i < 32; ++i) {
-            v0[i] = i+1;
-        }
-        vector<vector<double>> v(4, v0);
-        vector<double> v1(16);
-        for (size_t i = 0; i < 16; ++i) {
-            v1[i] = i+1;
-        }
-        vector<vector<double>> w(2, v1);
-        vector<double> v2(64);
-        for (size_t i = 0; i < 64; ++i) {
-            v2[i] = i+1;
-        }
-        vector<vector<double>> y(8, v2);
-        weightToPtxtOld(ptxt,13, w, 1,1,0,0, ecd);
-        cout << endl << "output weight vector: " << endl;
-        wgt = ecd.decode(ptxt);
-        printMessage(wgt);
-        cout << endl;
-        weightToPtxtOld(ptxt,13, w, 1,2,0,0, ecd);
-        cout << endl << "output weight vector: " << endl;
-        wgt = ecd.decode(ptxt);
-        printMessage(wgt);
-        cout << endl;
-        weightToPtxtOld(ptxt,13, v, 2,1,1,1, ecd);
-        cout << endl << "output weight vector: " << endl;
-        wgt = ecd.decode(ptxt);
-        printMessage(wgt);
-        cout << endl;
-        weightToPtxtOld(ptxt,13, y, 4,1,1,1, ecd);
-        cout << endl << "output weight vector: " << endl;
-        wgt = ecd.decode(ptxt);
-        printMessage(wgt);
-        cout << endl;
-        weightToPtxtOld(ptxt,13, v, 2,2,1,1, ecd);
-        cout << endl << "output weight vector: " << endl;
-        wgt = ecd.decode(ptxt);
-        printMessage(wgt);
-        cout << endl;
-
-        Message msg(log_slots);
-        for (size_t i = 0; i < num_slots; ++i) {
-            msg[i].real((double)((197*i*i) %256)/256 - 0.5);
-            msg[i].imag(0.0);
-        }
-        printMessage(msg);
-        cout << endl << "message vector: " << endl;
-        cout << "Encrypt ... ";
-        Ciphertext ctxt(context), ctxt_out(context);
-        enc.encrypt(msg, pack, ctxt);
-        cout << "done" << endl << endl;
-
-        cout << "Evaluating weight * ctxt" << endl;
-        eval.mult(ctxt, ptxt, ctxt_out);
-        cout << "done" << endl << endl;
-
-        cout << "Result ciphertext - level " << ctxt_out.getLevel()
-                  << endl
-                  << endl;
-
-        Message dmsg;
-        cout << "Decrypt ... ";
-        dec.decrypt(ctxt_out, sk, dmsg);
-        cout << "done" << endl;
-
+        Message dmsg(log_slots);
+        weightToPtxt(ptxt, 5, 1.0, 1, 2, 1, 1, ecd);
+        dmsg = ecd.decode(ptxt);
         cout.precision(2);
-        cout << endl << "Decrypted message : " << endl;
         printMessage(dmsg);
+        
+        weightToPtxt(ptxt, 5, 1.0, 2, 2, 1, 1, ecd);
+        dmsg = ecd.decode(ptxt);
+        cout.precision(2);
+        printMessage(dmsg);
+        
+        weightToPtxt(ptxt, 5, 1.0, 1, 1, 1, 1, ecd);
+        dmsg = ecd.decode(ptxt);
+        cout.precision(2);
+        printMessage(dmsg);
+        
+        weightToPtxt(ptxt, 5, 1.0, 2, 1, 1, 1, ecd);
+        dmsg = ecd.decode(ptxt);
+        cout.precision(2);
+        printMessage(dmsg);
+        
+        weightToPtxt(ptxt, 5, 1.0, 4, 1, 1, 1, ecd);
+        dmsg = ecd.decode(ptxt);
+        cout.precision(2);
+        printMessage(dmsg);
+
+        
+        
     }
 
     return 0;
