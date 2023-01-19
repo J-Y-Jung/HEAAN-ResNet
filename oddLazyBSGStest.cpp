a313fa27117c9c8478fad7d296b3cb8c06d0390e
altaria1 <114068321+altaria1@users.noreply.github.com>
2023-01-19 11:26:48 +0900

    Update oddLazyBSGStest.cpp

--- oddLazyBSGStest.cpp
+++ oddLazyBSGStest.cpp
@@ -169,45 +169,10 @@ int main() {
     std::cout << std::endl << "Decrypted result vector : " << std::endl;
     printMessage(dmsg2, false);
     
-  
-    vector<Ciphertext> ctxt_vec(64, ctxt);
-    vector<Ciphertext> ctxt_out(64, ctxt);
+    saveMessage(dmsg2, "app/HEAAN-ResNet/reluOut");
     
-    timer.start("method 1");
-    #pragma omp parallel for num_threads(64)
-    for(int i = 0 ; i < 64 ; ++i){
-        ApproxReLU(context, eval, ctxt_vec[i], ctxt_out[i]); 
-    }
-    timer.end();
-    
-    timer.start("method 2");
-    #pragma omp parallel for num_threads(80)
-    for(int i = 0 ; i < 40 ; ++i){
-        #pragma omp parallel num_threads(2)
-        {
-        ApproxReLU(context, eval, ctxt_vec[i], ctxt_out[i]); 
-        }
-    }
-    
-    #pragma omp parallel for num_threads(80)
-    for(int i = 40 ; i < 56 ; ++i){
-        #pragma omp parallel num_threads(5)
-        {
-        ApproxReLU(context, eval, ctxt_vec[i], ctxt_out[i]); 
-        }
-    }
-    
-    #pragma omp parallel for num_threads(80)
-    for(int i = 56 ; i < 64 ; ++i){
-        #pragma omp parallel num_threads(10)
-        {
-        ApproxReLU(context, eval, ctxt_vec[i], ctxt_out[i]); 
-        }
-    }
-    
-    
-    timer.end();
     return 0;
+  
 
 }
 
