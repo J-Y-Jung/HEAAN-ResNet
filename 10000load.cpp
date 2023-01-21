0ee84b17b4320da58895cbbc343b288b27e3a9fe
altaria1 <114068321+altaria1@users.noreply.github.com>
2023-01-21 23:17:49 +0900

    Update and rename 10000.cpp to 10000load.cpp

--- 10000load.cpp
+++ 10000load.cpp
@@ -969,11 +969,14 @@ int main() {
     // Second convolution
     cout << "block3conv1 ..." << endl;
     timer.start(" block3conv1 ");
-    vector<vector<Ciphertext>> ctxt_block3conv1_out;
+    vector<vector<Ciphertext>> ctxt_block3conv1_out(16, vector<Ciphertext>(16, ctxt_init));
+    
+    #pragma omp parallel for num_threads(80)
     for (int i = 0; i < 16; ++i) {
-        vector<Ciphertext> ctxt_conv_out2_allch_bundle;
+        #pragma omp parallel num_threads(80)
+        {
         ctxt_conv_out2_allch_bundle = Conv(context, pack, eval, 32, 1, 1, 16, 16, ctxt_block3relu0_out[i], block3conv1multiplicands16_16_3_3);
-        ctxt_block3conv1_out.push_back(ctxt_conv_out2_allch_bundle);
+        }
     }
 
     addBNsummands(context, eval, ctxt_block3conv1_out, block3conv1summands16, 16, 16);
