ff9f41e1b7797bc8f1b32ce08b6c3580a72121b9
altaria1 <114068321+altaria1@users.noreply.github.com>
2023-01-28 12:02:56 +0900

    Update imageEncode.hpp

--- imageEncode.hpp
+++ imageEncode.hpp
@@ -14,21 +14,12 @@ namespace {
 void txtreader(vector<double>& kernel, const string filename) {
 
     string line;
-    //ifstream input_file(file_path);
     ifstream input_file(filename);
-    /*if (!input_file.is_open()) {
-        cerr << "Could not open the file - '"
-            << filename << "'" << endl;
-        return EXIT_FAILURE;
-    }*/
 
     while (getline(input_file, line)) {
         double temp = stod(line);
         kernel.push_back(temp);
     }
-
-    /*for (const auto& i : kernel)
-        cout << i << endl;*/
 
     input_file.close();
     return;
@@ -41,13 +32,6 @@ vector<double> slice(const std::vector<double>& input, int a, int b) {
     return std::vector<double>(first, last);
 }
 
-//vector<int> merge(vector<int> a, vector<int> b) {
-//    vector<int> c;
-//    c.reserve(a.size() + b.size()); // preallocate memory
-//    c.insert(c.end(), a.begin(), a.end());
-//    c.insert(c.end(), b.begin(), b.end());
-//}
-
 void imageCompiler(Context context, KeyPack pack, Encryptor enc, u64 level, vector<double>& image, vector<Ciphertext>& output) {
 
     Message msg1(15), msg2(15), msg3(15);
@@ -56,12 +40,11 @@ void imageCompiler(Context context, KeyPack pack, Encryptor enc, u64 level, vect
     
     
     Ciphertext ctxt1(context), ctxt2(context), ctxt3(context);
-    #pragma omp parallel for
+    
     for (int i = 0; i < 32; ++i) {
         vector<double> temp1 = slice(image, 3072 * i, 3072 * i + 1024);
         vector<double> temp2 = slice(image, 3072 * i + 1024, 3072 * i + 2048);
         vector<double> temp3 = slice(image, 3072 * i + 2048, 3072 * i + 3072);
-
 
         input1.insert(input1.end(), temp1.begin(), temp1.end());
         input2.insert(input2.end(), temp2.begin(), temp2.end());
@@ -83,10 +66,9 @@ void imageCompiler(Context context, KeyPack pack, Encryptor enc, u64 level, vect
     
 
     Plaintext ptxt1(context), ptxt2(context), ptxt3(context);
-
+    
     enc.encrypt(msg1, pack, output[0], level, 0);
     enc.encrypt(msg2, pack, output[1], level, 0);
     enc.encrypt(msg3, pack, output[2], level, 0);
-
     
 }
